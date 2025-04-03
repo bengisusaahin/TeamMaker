d8370e773e5af4ebbcbd68f3185bb2edad91ec6b
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2025-04-03 20:07:59 +0300

    feat: handle deprecated codes and missing permission

--- data/src/main/java/com/yusuf/data/repository/LocationRepositoryImpl.kt
+++ data/src/main/java/com/yusuf/data/repository/LocationRepositoryImpl.kt
@@ -1,9 +1,12 @@
 package com.yusuf.data.repository
 
-import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.location.Location
+import android.os.Build
+import android.util.Log
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.yusuf.domain.repository.LocationRepository
@@ -13,6 +16,8 @@ import kotlinx.coroutines.withContext
 import java.util.Locale
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 @Singleton
 class LocationRepositoryImpl @Inject constructor(
@@ -22,16 +27,44 @@ class LocationRepositoryImpl @Inject constructor(
     private val fusedLocationProviderClient : FusedLocationProviderClient =
         LocationServices.getFusedLocationProviderClient(context)
 
-    @SuppressLint("MissingPermission")
+
     override suspend fun getLocation(): Location? {
+
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.e("LocationRepository", "Location permission not granted")
+            return null
+        }
+
         return fusedLocationProviderClient.lastLocation.await()
     }
 
     override suspend fun getLocationName(latitude: Double, longitude: Double): String {
         return withContext(Dispatchers.IO) {
-            val geocoder = Geocoder(context, Locale.getDefault())
-            val addresses = geocoder.getFromLocation(latitude, longitude, 1)
-            addresses?.firstOrNull()?.locality ?: "Unknown city"
+            try {
+                val geocoder = Geocoder(context, Locale.getDefault())
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    suspendCoroutine { cont ->
+                        geocoder.getFromLocation(latitude, longitude, 1) { addresses ->
+                            cont.resume(addresses.firstOrNull()?.locality ?: "Unknown city")
+                        }
+                    }
+                } else {
+                    suspendCoroutine { cont ->
+                        @Suppress("DEPRECATION")
+                        val addresses = geocoder.getFromLocation(latitude, longitude, 1)
+                        cont.resume(addresses?.firstOrNull()?.locality ?: "Unknown city")
+                    }
+                }
+            } catch (e: Exception) {
+                Log.e("LocationRepository", "Error fetching location name", e)
+                "Error fetching location name $e"
+            }
         }
     }
+
 }
